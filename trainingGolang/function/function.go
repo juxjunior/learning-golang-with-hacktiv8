5783376c0149b02d66d57517949d028d2d86728f
P93785-qa <imanuel.junior@pegadaian.co.id>
2023-11-20 23:45:33 +0700

    rebase

--- trainingGolang/function/function.go
+++ trainingGolang/function/function.go
@@ -11,7 +11,7 @@ func greet(name string, age int8) {
 	fmt.Printf("Hello there! My name is %s and I'm %d years old", name, age)
 }
 
-func salam(name, address string){
+func salam(name, address string) {
 	fmt.Println("Hello there! My name is", name)
-	fmt.Println("I live in", address)
+	fmt.Println("I live in ", address)
 }
