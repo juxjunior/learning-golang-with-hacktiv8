9f6942c0b027e9cf5097fb98cc23f38670378f22
P93785-qa <imanuel.junior@pegadaian.co.id>
2023-11-21 12:00:24 +0700

    Completed Function

--- trainingGolang/function/function.go
+++ trainingGolang/function/function.go
@@ -11,6 +11,10 @@ func main() {
 	salam("Jux", "Jakpus")
 	mainUntukSalam2()
 	mainUntukCalculate()
+	mainUntukCalculate2()
+	mainUntukPrint()
+	mainUntukSum()
+	mainUntukProfile()
 }
 
 // contoh sederhana
@@ -57,3 +61,68 @@ func calculate(d float64) (float64, float64) {
 }
 
 // contoh menggunakan function predefined return values
+
+func mainUntukCalculate2() {
+	var diameter2 float64 = 15
+	var area, circumference = calculate2(diameter2)
+
+	fmt.Println("Area:", area)
+	fmt.Println("Circumference:", circumference)
+}
+
+func calculate2(d float64) (area float64, circumference float64) {
+	//Menghitung luas
+	area = math.Pi * math.Pow(d/2, 2)
+	//Menghitung keliling
+	circumference = math.Pi * d
+
+	return
+}
+
+// contoh menggunakan varadic function #1
+func mainUntukPrint() {
+	studentLists := print("John", "Ethan", "Hunt", "Wick", "Claude")
+	fmt.Printf("%#v", studentLists)
+}
+
+func print(names ...string) []map[string]string {
+	var result []map[string]string
+
+	for i, v := range names {
+		key := fmt.Sprintf("student%d:", i+1)
+		temp := map[string]string{
+			key: v,
+		}
+		result = append(result, temp)
+	}
+	return result
+}
+
+// contoh menggunakan varadic function #2
+func mainUntukSum() {
+	numberLists := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+	result := sum(numberLists...)
+	fmt.Println("\nResult:", result)
+}
+
+func sum(numbers ...int) int {
+	total := 0
+
+	for _, v := range numbers {
+		total += v
+	}
+	return total
+}
+
+// contoh menggunakan varadic function #3
+func mainUntukProfile() {
+	profile("Udang", "Geprek", "Sambal", "Pedas", "Pete")
+}
+
+func profile(name string, favFoods ...string) {
+	mergeFavFoods := strings.Join(favFoods, ",")
+
+	fmt.Println("\nHalo, halo Saya", name)
+	fmt.Println("\nSaya suka makan", mergeFavFoods)
+}
